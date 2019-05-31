afc1baf9b3aca812c72d15581d2282ad55b4a933
Benjamin Zhang <benjaminzhang@nestmk12.net>
2019-05-31 19:09:37 +0000

    Initial commit

---
+++ spetch.js
@@ -0,0 +1,68 @@
+let snake;
+let rez = 20;
+let food;
+let w;
+let h;
+let r = "Press R to restart.";
+
+function setup(){
+    createCanvas(400, 400);
+    w = floor(width / rez);
+    h = floor(height / rez);
+    frameRate(5);
+    snake = new Snake();
+    foodLocation();
+    textSize(0.5);
+      }
+
+function foodLocation() {
+  let x = floor(random(w));
+  let y = floor(random(h));
+  food = createVector(x, y);
+
+}
+function keyPressed() {
+  if(keyCode === LEFT_ARROW) {
+    snake.setDir(-1, 0);
+  } else if (keyCode === RIGHT_ARROW) {
+  	snake.setDir(1, 0);
+  } else if (keyCode === DOWN_ARROW) {
+  	snake.setDir(0, 1);
+  } else if (keyCode === UP_ARROW) {
+  	snake.setDir(0, -1);
+  } else if (key == ' ') {
+  	snake.grow();
+  }
+  if(keyCode === 82){
+    background(120);
+    foodLocation();
+    snake = new Snake();
+    loop();
+  }
+}
+function draw(){
+    background(120);
+    fill(65, 45, 87);
+      scale(rez);
+
+      if (snake.eat(food)) {
+         foodLocation();
+      }
+      snake.update();
+      snake.show();
+
+      if (snake.endGame()) {
+        background(255)
+        fill('black')
+        text(r, 1, 1);
+        noLoop();
+      }
+
+
+
+      noStroke();
+      fill(255);
+      ellipse(food.x, food.y, 1, 1);
+
+      }
+
